87a37cfd8984dfa05f6d3656f2755ced4e59570c
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-11 16:14:05 +0100

    dice game

--- js/script.js
+++ js/script.js
@@ -16,3 +16,19 @@ button.addEventListener('click', function(){
         result.innerHTML = 'Access denied';
     }
 })
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let userNum = random(1, 6);
+let computerNum = random(1, 6);
+
+let dice = document.getElementById('winner');
+if(userNum > computerNum){
+    dice.innerHTML = `L'utente ha vinto con il numero ${userNum} rispetto al numero del computer ${computerNum}`;
+} else if(userNum < computerNum){
+    dice.innerHTML = `Il computer ha vinto con il numero ${computerNum} rispetto al numero dell'utente ${userNum}`;
+} else {
+    dice.innerHTML = `C'è stato un pareggio con il numero ${userNum}`;
+}
